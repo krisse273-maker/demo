9ce6e2536c92dc75c52f744c587c4e8d074ba3a4
krisse273-maker <krisse273@gmail.com>
2026-01-30 15:38:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("JavaScript är laddat och klar för användning!");
+    // Du kan lägga till funktioner för registreringsformuläret här
+});
