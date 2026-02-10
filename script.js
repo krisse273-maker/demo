78b994a18b2acb9af56ca09c3792e0c07169f689
krisse273-maker <krisse273@gmail.com>
2026-02-10 22:14:02 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// script.js
-
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("JavaScript är laddat och klar för användning!");
-    // Du kan lägga till funktioner för registreringsformuläret här
-});
